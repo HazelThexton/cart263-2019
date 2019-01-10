8d917cc27b6c19524d7e134c2581deb1a4c75af4
Hazel Thexton <hazelthexton@videotron.ca>
2019-01-10 15:43:09 -0500

    EX2: Created an update function and added it to draw

--- assignments/assignment2/js/Food.js
+++ assignments/assignment2/js/Food.js
@@ -23,9 +23,26 @@ class Food extends Agent {
   //
   // Set position to a random location on the canvas
   // Set the size to a random size within the limits
+  // Set the velocity to a random value based on max speed
   reset() {
     this.x = random(0,width);
     this.y = random(0,height);
     this.size = random(this.minSize,this.maxSize);
+    this.vx = random(-this.maxSpeed,this.maxSpeed);
+    this.vy = random(-this.maxSpeed,this.maxSpeed);
+  }
+
+  // update()
+  //
+  // Set the velocity randomly based on probability and updates position
+  update() {
+    this.x = constrain(this.x + this.vx,0,width);
+    this.y = constrain(this.y + this.vy,0,height);
+
+    // Changes velocity 5% of the time
+    if (random() <= 0.05){
+      this.vx = (random(-this.maxSpeed,this.maxSpeed));
+      this.vy = (random(-this.maxSpeed,this.maxSpeed));
+    }
   }
 }
