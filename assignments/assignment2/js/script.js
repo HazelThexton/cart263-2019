5b937f2a28666decebf0d12c928e0a3c5d5e92ce
Hazel Thexton <hazelthexton@videotron.ca>
2019-01-10 16:14:30 -0500

    EX2: Removed scroll bar

--- assignments/assignment2/js/script.js
+++ assignments/assignment2/js/script.js
@@ -7,7 +7,7 @@ Hazel Thexton
 
 An Object-Oriented version of the Circle Eater program.
 The player moves a circle around with the mouse.
-Another circle represents food which the player eats by overlapping.
+Multiple circles represent food which the player eats by overlapping.
 The player circle shrinks over time, but grows when it eats.
 
 ******************/
@@ -27,7 +27,6 @@ let food = [];
 // preload()
 //
 // Not needed
-
 function preload() {
 
 }
@@ -36,10 +35,10 @@ function preload() {
 // setup()
 //
 // Create the canvas, avatar, and food, disable the cursor
-
 function setup() {
   createCanvas(windowWidth,windowHeight);
   avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME);
+  // Creates 5 foods and adds them to the food array
   for (let i=0; i < 5; i++){
     food.push(new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,FOOD_MAX_SPEED));
   }
@@ -51,7 +50,6 @@ function setup() {
 // Clear the background
 // Update the avatar and check for eating
 // Display the avatar and food
-
 function draw() {
   background(0);
 
