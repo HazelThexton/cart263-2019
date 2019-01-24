9ac00c8c312575fe1a4006cc64ae6db8220dbaba
Hazel Thexton <hazelthexton@videotron.ca>
2019-01-24 15:42:30 -0500

    P1: Text formatting edits, made angle value an integer
    Added p5 library to access integer function, for more concise angle values.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,70 +9,49 @@ Sisyphus
 
 ******************/
 
-// Variable tracking which screen you are on
-let currentScreen = 0;
-
-// A place to store the jQuery selection of redacted spans
-let $redactSpans;
-// A place to store the jQuery selection of secret spans
-let $secretSpans;
+// A place to store the angle of the mobile device
+let $angle;
 
 
 // When the document is loaded we call the setup function
 $(document).ready(setup);
 
 window.addEventListener('deviceorientation', function(event) {
-  console.log('angle: ' + event.beta);
+  // Gives us a value for the angle of the mobile device on the x axis
+  // and stores it in the angle variable
+  $angle = $(int(event.beta));
+  console.log($angle);
 });
 
 // setup()
 //
-// Sets the click handler and starts the time loop
+//
 function setup() {
   if (window.orientation == 0){
-    $("#portrait-text").text( "Turn your device!" );
+    $("#portrait-text").show()
     $("span.game-elements").hide();
   }
   else {
-    $("#portrait-text").text(" ");
+    $("#portrait-text").hide();
     $("span.game-elements").show();
-
   }
-  // Set an interval of 500 milliseconds to update the state of the page
   window.update();
 };
 
 // update()
 //
-// Update is called every 500 milliseconds and it updates all the spans on the page
-// using jQuery's each() function which calls the specified function on _each_ of the
-// elements in the selection
+// Updates the page
 function update() {
   $(window).on("orientationchange", function(event) {
     if (window.orientation == 0){
-      $("#portrait-text").text( "Turn your device!" );
+      $("#portrait-text").show()
       $("span.game-elements").hide();
     }
     else {
-      $("#portrait-text").text(" ");
+      $("#portrait-text").hide();
       $("span.game-elements").show();
-
     }
   });
-
-}
-
-// updateSpan()
-//
-// With a probability of 10% it unblanks the current span by removing the
-// redacted class and adding the revealed class. Because this function is called
-// by each(), "this" refers to the current element that each has selected.
-function updateSpan() {
-  let r = Math.random();
-  if (r < 0.1) {
-    $(this).removeClass('redacted');
-    $(this).addClass('revealed');
-  }
 }
 
 // spanMouseover()
