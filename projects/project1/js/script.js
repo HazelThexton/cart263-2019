ab965937566af18dd639015db68882df50c40bb0
Hazel Thexton <hazelthexton@videotron.ca>
2019-01-31 20:19:11 -0500

    P1: Changed scroll speed

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -95,7 +95,7 @@ function roll() {
 
   timePerFrame = map(speed,0,30,100,50);
   requestAnimationFrame(step);
-  scrollSpeed = map(speed,0,100,0,60);
+  scrollSpeed = speed;
   bgScroll();
 
   // Moves the boulder left (if it's within the screen)
