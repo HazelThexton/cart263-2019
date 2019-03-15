faf5925cc4d5109b1934695f234e10ea4f78f1fa
Hazel Thexton <hazelthexton@videotron.ca>
2019-03-14 23:23:08 -0400

    Revert "P2: Removed onscreenText file (superfluous)"
    This reverts commit 8688a7a3cb8ccc9106dc1b9e3eea120938e970f1.

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -6,6 +6,7 @@ Youtube Storytime
 Hazel Thexton
 
 It is highly recommended to use HEADPHONES (to prevent responsiveVoice from
+<<<<<<< HEAD
   triggering annyang) and FIREFOX (to avoid a microphone input bug in google chrome)
 
   video from
@@ -34,6 +35,83 @@ It is highly recommended to use HEADPHONES (to prevent responsiveVoice from
     video.hide();
 
   }
+=======
+triggering annyang) and FIREFOX (to avoid a microphone input bug in google chrome)
+
+video from
+
+https://www.youtube.com/watch?v=YfUNPdxP6mo&t=35s
+
+******************/
+
+let startTextImage;
+let startScreen = true;
+let helpActive = false;
+let video;
+let mic;
+let font;
+let textArea;
+
+// preload()
+//
+// Description of preload
+function preload() {
+  startTextImage = loadImage("assets/images/startTextImage.png");
+video = createVideo(["assets/images/video.mp4"]);
+video.elt.muted = true;
+video.loop();
+video.hide();
+
+}
+
+// setup()
+//
+// Sets up the canvas, creates text objects and initialies annyang
+function setup() {
+  createCanvas(windowWidth,windowHeight, WEBGL);
+  // Create an Audio input
+  mic = new p5.AudioIn();
+  // Start the Audio Input.
+  mic.start();
+
+  if (annyang) {
+    // Defines voice commands
+    var commands = {
+      'tell me a story': story,
+      'tell me a long story': function() {
+        story(long)
+      },
+      'tell me a short story': function() {
+        story(short)
+      },
+      'stop (talking)': function() {
+        responsiveVoice.pause();
+      },
+      'keep talking': function() {
+        responsiveVoice.resume();
+      },
+      'what do i do': help,
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening.
+    annyang.start();
+  }
+}
+
+// draw()
+//
+// enables the help screen by default
+function draw() {
+  background(0);
+  if (startScreen) {
+startText ();
+    if (mouseIsPressed){
+      helpActive = true;
+      startScreen = false;
+>>>>>>> parent of 8688a7a... P2: Removed onscreenText file (superfluous)
 
   // setup()
   //
@@ -97,6 +175,7 @@ It is highly recommended to use HEADPHONES (to prevent responsiveVoice from
     pop();
   }
 
+<<<<<<< HEAD
   // story()
   //
   // Creates a story based on our grammar and on the length chosen by the user,
@@ -105,19 +184,33 @@ It is highly recommended to use HEADPHONES (to prevent responsiveVoice from
     console.log('working');
     // If the user just asked for a story without specifying length, it's selected randomly
     if (lengthOrName != 'short' || lengthOrName != 'long'){
+=======
+function startText () {
+  normalMaterial();
+  texture(startTextImage);
+  push();
+  plane(500,105);
+  pop();
+}
+
+// story()
+//
+// Creates a story based on our grammar and on the length chosen by the user,
+// then displays and/or reads it
+function story(length) {
+
+// If the user just asked for a story without specifying length, it's selected randomly
+    if (length === undefined){
+>>>>>>> parent of 8688a7a... P2: Removed onscreenText file (superfluous)
       if (random <= 0.5) {
-        let length = short;
+        length = short;
       }
       else {
-        let length = long;
-      }
-      // If the argument passed from annyang isn't a length OR undefined, it's a name,
-      // which we push to the grammar to get a story featuring that name
-      if (lengthOrName != undefined){
-        grammar.pushRules(name, [lengthOrName]);
+        length = long;
       }
     }
 
+<<<<<<< HEAD
     console.log(lengthOrName);
 
     // Generates a trace, aka a possible output of a tracery grammar
@@ -163,3 +256,50 @@ It is highly recommended to use HEADPHONES (to prevent responsiveVoice from
     torus(100+torusSize,50);
     pop();
   }
+=======
+  // Generates a trace, aka a possible output of a tracery grammar
+  let grammar = tracery.createGrammar(length);
+
+  //if (yourName != noname){
+  //  grammar.pushRules(name, [yourName]);
+//  }
+
+  let trace = grammar.flatten('#origin#');
+  // Has our responsiveVoice speak the trace out loud
+  responsiveVoice.speak(trace,'UK English Female');
+
+}
+
+// help()
+//
+// Displays a list of the voice commands a user can use to interact
+function help() {
+  // Clears the screen
+  background(255);
+  // If text is disabled, responsiveVoice reads out the instructions
+  responsiveVoice.speak("say 'what do i do' for help. say 'tell me a story' for a story. You can also ask for a long or short story. Say 'Stop talking' or 'keep talking' to pause or resume the story.",'UK English Female');
+
+  helpActive = false;
+}
+
+function videoTorus() {
+  // Basic ambient lighting
+  ambientLight(255,255,255);
+  // Additional directional light to add some dimension
+  directionalLight(255, 255, 255, 1, -1, 0);
+  // Specular material reflects light
+  specularMaterial(0,0,0);
+  // Applies a weird kids' video as the texture
+  texture(video);
+  // The size of the video torus increases proportionally to noise detected by the mic
+  let torusSize = mic.getLevel()*500;
+  // Mild constant rotation in all directions
+ push();
+ rotateZ(frameCount * 0.02);
+ rotateX(frameCount * 0.05);
+ rotateY(frameCount * 0.01);
+ // The 'thickness' of the torus always stays the same for a stretching/squashing effect
+ torus(100+torusSize,50);
+ pop();
+}
+>>>>>>> parent of 8688a7a... P2: Removed onscreenText file (superfluous)
