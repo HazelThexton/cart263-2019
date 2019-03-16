bbe61eb8fd9e2726b7f2b02cc31423ca1c478f32
Hazel Thexton <hazelthexton@videotron.ca>
2019-03-16 19:07:47 -0400

    EX7: Removed ability to restart synth with multiple clicks

--- assignments/assignment7/js/script.js
+++ assignments/assignment7/js/script.js
@@ -19,6 +19,8 @@ const ATTACK = 0.1;
 // Release time for a note (in seconds)
 const RELEASE = 0.1;
 
+let started = false;
+
 // We need an array of the possible notes to play as frequencies (in Hz)
 // A Major =  A, B, C♯, D, E, F♯, and G♯
 // We can get the frequencies of these notes from THE INTERNET, e.g.
@@ -42,7 +44,7 @@ let patternIndex = 0;
 
 // setup()
 //
-// Creat canvas, set up the synth and sound files.
+// Create canvas, set up the synth and sound files.
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
@@ -85,10 +87,13 @@ function setup() {
 // Using this to start the note and drum sequences to get around
 // user interaction (and to give the files time to load)
 function mousePressed() {
-  // Start an interval for the notes
-  setInterval(playNote,NOTE_TEMPO);
-  // Start an interval for the drums
-  setInterval(playDrum,DRUM_TEMPO);
+  if (started === false) {
+    // Start an interval for the notes
+    setInterval(playNote,NOTE_TEMPO);
+    // Start an interval for the drums
+    setInterval(playDrum,DRUM_TEMPO);
+  }
+  started = true;
 }
 
 // playNote
