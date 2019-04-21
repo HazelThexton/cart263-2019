8697629da9cd256af41422e286df78f12e1623bf
Hazel Thexton <hazelthexton@videotron.ca>
2019-04-21 15:17:55 -0400

    P3: Added Pizzicato library

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -13,8 +13,7 @@ author, and this description to match your project!
 let imgElement;
 let inputElement;
 let mat;
-
-
+let $circlesButton;
 
 // Calls setup when the document is ready
 $(document).ready(setup);
@@ -22,17 +21,16 @@ $(document).ready(setup);
 // setup()
 //
 // Description of setup
-
 function setup() {
   imgElement = document.getElementById('imageSrc');
   inputElement = document.getElementById('fileInput');
+  $circlesButton = $('.circlesButton');
   onOpenCvReady();
-
 }
 
 function onOpenCvReady() {
-
-  document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+  // Once OpenCV is ready we display a prompt
+  document.getElementById('status').innerHTML = 'Load an image to make it sing!';
   // changes the location of the image to the url of the input
   inputElement.onchange = function() {
     imgElement.src = URL.createObjectURL(event.target.files[0]);
@@ -42,24 +40,26 @@ function onOpenCvReady() {
     cv.imshow('imageCanvas', image);
     image.delete();
   };
-  document.getElementById('circlesButton').onclick = function() {
-    let srcMat = cv.imread('imageCanvas');
-    let displayMat = srcMat.clone();
-    let circlesMat = new cv.Mat();
-    cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
-    cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
-    for (let i = 0; i < circlesMat.cols; ++i) {
-        let x = circlesMat.data32F[i * 3];
-        let y = circlesMat.data32F[i * 3 + 1];
-        let radius = circlesMat.data32F[i * 3 + 2];
-        let center = new cv.Point(x, y);
-        cv.circle(displayMat, center, radius, [0, 0, 0, 255], 3);
-    }
-    cv.imshow('imageCanvas', displayMat);
-  };
+$circlesButton.on('click',detectCircles);
 }
 
-
+function detectCircles() {
+  $(this).text('Loading...');
+  let srcMat = cv.imread('imageCanvas');
+  let displayMat = srcMat.clone();
+  let circlesMat = new cv.Mat();
+  cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
+  cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 90, 90, 60, 0, 0);
+  for (let i = 0; i < circlesMat.cols; ++i) {
+      let x = circlesMat.data32F[i * 3];
+      let y = circlesMat.data32F[i * 3 + 1];
+      let radius = circlesMat.data32F[i * 3 + 2];
+      let center = new cv.Point(x, y);
+      cv.circle(displayMat, center, radius, [0, 0, 0, 255], 3);
+  }
+  cv.imshow('imageCanvas', displayMat);
+  $(this).text('Done!');
+}
 
 // draw()
 //
