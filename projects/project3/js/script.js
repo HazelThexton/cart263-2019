f8ea27fd76a9f2d371ecaf5f627b0c99701062db
Hazel Thexton <hazelthexton@videotron.ca>
2019-04-20 23:45:22 -0400

    P3: Successfully implemented circle detection

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -14,6 +14,8 @@ let imgElement;
 let inputElement;
 let mat;
 
+
+
 // Calls setup when the document is ready
 $(document).ready(setup);
 
@@ -22,23 +24,42 @@ $(document).ready(setup);
 // Description of setup
 
 function setup() {
-onOpenCvReady();
-   imgElement = document.getElementById("imageSrc");
-   inputElement = document.getElementById("fileInput");
-   inputElement.addEventListener('change', (e) => {
-     imgElement.src = URL.createObjectURL(e.target.files[0]);
-   }, false);
-   imgElement.onload = function() {
-     mat = cv.imread(imgElement);
-     cv.imshow('canvasOutput', mat);
-     mat.delete();
-   };
+  imgElement = document.getElementById('imageSrc');
+  inputElement = document.getElementById('fileInput');
+  onOpenCvReady();
 
 }
 
 function onOpenCvReady() {
+
   document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+  // changes the location of the image to the url of the input
+  inputElement.onchange = function() {
+    imgElement.src = URL.createObjectURL(event.target.files[0]);
+  };
+  imgElement.onload = function() {
+    let image = cv.imread(imgElement);
+    cv.imshow('imageCanvas', image);
+    image.delete();
+  };
+  document.getElementById('circlesButton').onclick = function() {
+    let srcMat = cv.imread('imageCanvas');
+    let displayMat = srcMat.clone();
+    let circlesMat = new cv.Mat();
+    cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
+    cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
+    for (let i = 0; i < circlesMat.cols; ++i) {
+        let x = circlesMat.data32F[i * 3];
+        let y = circlesMat.data32F[i * 3 + 1];
+        let radius = circlesMat.data32F[i * 3 + 2];
+        let center = new cv.Point(x, y);
+        cv.circle(displayMat, center, radius, [0, 0, 0, 255], 3);
+    }
+    cv.imshow('imageCanvas', displayMat);
+  };
 }
+
+
 
 // draw()
 //
