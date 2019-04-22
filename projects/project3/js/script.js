138d60ffde25a7afc2bee145b202fbfaeb5d4e47
Hazel Thexton <hazelthexton@videotron.ca>
2019-04-21 20:36:53 -0400

    P3: Commenting edits

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -2,10 +2,12 @@
 
 /*****************
 
-Synaesthetics
+Synthesthesia
 Hazel Thexton
 
 Turns images into sound.
+
+https://scotch.io/tutorials/introduction-to-computer-vision-in-javascript-using-opencvjs
 
 ******************/
 
@@ -13,29 +15,26 @@ Turns images into sound.
 // HTML elements
 let imgElement;
 let inputElement;
-let mat;
 let $circlesButton;
-
-// Whether the image has been read
-let imageLoaded = false;
-
+// OpenCV matrices
+let srcMat;
+let displayMat;
+let circlesMat;
+// Whether circles have been detected
+let circlesDetected = false;
 // Note frequency
 let xyFrequency = [];
 let xyIndex = 0;
-
 // Note tempo
 let radiusTempo = [];
 let radiusIndex = 0;
 let tempo;
-
-
 // Attack time for a note (in seconds)
 const ATTACK = 0.1;
 // Release time for a note (in seconds)
 const RELEASE = 0.1;
 // Whether the sound has started
 let started = false;
-
 // The synth
 let synth;
 
@@ -70,9 +69,12 @@ function onOpenCvReady() {
   // Once OpenCV is ready we display a prompt
   document.getElementById('status').innerHTML = 'Load an image to make it sing!';
 
-  // Once user has input an image, hanges the location of the image to the url of the input
+  // Once user has input an image, changes the location of the image to the url of the input
+  // and resets the button and synth
   inputElement.onchange = function() {
     imgElement.src = URL.createObjectURL(event.target.files[0]);
+    // Resets the button and synth
+    reset();
   };
   // Once the image has loaded, displays the image on the canvas
   imgElement.onload = function() {
@@ -84,49 +86,69 @@ function onOpenCvReady() {
   $circlesButton.on('click',detectCircles);
 }
 
+// detectCircles()
+//
+// Detects circles in the image and displays them over top of it
 function detectCircles() {
-  xyFrequency.length = 0;
-  radiusTempo.length = 0;
-  $(this).text('Loading...');
-  let srcMat = cv.imread('imageCanvas');
-  let displayMat = srcMat.clone();
-  let circlesMat = new cv.Mat();
-  let dsize = new cv.Size(0, 0);
+  // Unbinds the event handler to avoid repeat clicks
+  $(this).unbind( "click" );
+  // Assigns our matrices- a source, an output, and one for the circle detection
+  srcMat = cv.imread('imageCanvas');
+  displayMat = srcMat.clone();
+  circlesMat = new cv.Mat();
+  // Converts our image to greyscale and resizes it for easier circle detection
   cv.cvtColor(srcMat, srcMat, cv.COLOR_RGBA2GRAY);
-cv.resize(srcMat, srcMat, dsize, 0.2, 0.2, cv.INTER_AREA);
-  cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 70, 70, 10, 0, 0);
+  cv.resize(srcMat, srcMat, new cv.Size(0, 0), 0.1, 0.1, cv.INTER_AREA);
+  // This is the actual function which detects the circles and stores their information in the matrix
+  cv.HoughCircles(srcMat, circlesMat, cv.HOUGH_GRADIENT, 1, 45, 75, 40, 0, 0);
+  // Grabs x, y and radius from the matrix and draws each circle in its
+  // respective location (with a x10 multiplier to undo the resize)
   for (let i = 0; i < circlesMat.cols; ++i) {
-    let x = circlesMat.data32F[i * 3]*5;
-    let y = circlesMat.data32F[i * 3 + 1]*5;
-    let radius = circlesMat.data32F[i * 3 + 2]*5;
+    let x = circlesMat.data32F[i * 3]*10;
+    let y = circlesMat.data32F[i * 3 + 1]*10;
+    let radius = circlesMat.data32F[i * 3 + 2]*10;
     let center = new cv.Point(x, y);
     cv.circle(displayMat, center, radius, [0, 0, 0, 255], 3);
+    // For each circle, maps the x*y to frequency (so the lowest freq circle would be
+    // in the top left and vice versa) and the radius to tempo (so the larger
+    // a circle is, the longer the note lasts) and pushes this to corresponding arrays
     xyFrequency.push(map(x*y,0,windowWidth*windowHeight,200,500,true));
-    radiusTempo.push(map(radius,0,300,50,300));
-    console.log("location " + xyFrequency[i]);
+    radiusTempo.push(map(radius,0,300,5,200,true));
+    console.log(xyFrequency[i]);
   }
+  // Displays the completed image, with circles
   cv.imshow('imageCanvas', displayMat);
+  // If no circles were detected (so the frequency array is empty), a message
+  // is displayed and it doesn't attempt to play sound (otherwise we would get an error)
   if (xyFrequency.length === 0) {
     $(this).text('Please try another image.');
   }
+  // Otherwise, another message displays and we trip a variable which allows
+  // the sound to play
   else {
     $(this).text('Done!');
-    imageLoaded = true;
+    circlesDetected = true;
   }
 }
 
-
-
+// reset()
+//
+// resets the freq and tempo arrays, the button text, and pauses the synth
+function reset() {
+  synth.pause();
+  xyFrequency.length = 0;
+  radiusTempo.length = 0;
+  $circlesButton.text('click here');
+}
 
 // draw()
 //
 // Description of draw()
-
 function draw() {
   if (started === false) {
-    if (imageLoaded === true) {
+    if (circlesDetected === true) {
       started = true;
-        notes();
+      notes();
     }
   }
 }
@@ -139,9 +161,8 @@ function notes() {
 
 // playNote
 //
-// Chooses a random frequency and assigns it to the synth
+// Assigns a frequency from the array
 function playNote() {
-
   // Pick a random frequency from the array
   let frequency = xyFrequency[xyIndex];
   // Set the synth's frequency
